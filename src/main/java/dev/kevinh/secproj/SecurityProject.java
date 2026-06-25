7711a1cbf36e4b4673374c1a88f1749ea3e55c58
Kevin He <kevin@lyratechnologies.com.au>
2026-06-25 19:50:12 +1000

    example item

--- src/main/java/dev/kevinh/secproj/SecurityProject.java
+++ src/main/java/dev/kevinh/secproj/SecurityProject.java
@@ -7,14 +7,15 @@ import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import dev.kevinh.secproj.item.ModItems;
+
 public class SecurityProject implements ModInitializer {
   public static final String MOD_ID = "secproj";
-
   public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
   @Override
   public void onInitialize() {
-    LOGGER.info("Hello Fabric world!");
+    ModItems.registerModItems();
   }
 
   public static Identifier id(String path) {
--- src/main/java/dev/kevinh/secproj/item/ModItems.java
+++ src/main/java/dev/kevinh/secproj/item/ModItems.java
@@ -1,5 +1,26 @@
 package dev.kevinh.secproj.item;
 
+import dev.kevinh.secproj.SecurityProject;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
 public class ModItems {
 
+  public static final Item EXAMPLE_ITEM = registerItem("example_item", new Item(new Item.Settings()));
+
+  private static Item registerItem(String name, Item item) {
+    return Registry.register(Registries.ITEM, Identifier.of(SecurityProject.MOD_ID, name), item);
+  }
+
+  public static void registerModItems() {
+    SecurityProject.LOGGER.info("Registering Mod items for " + SecurityProject.MOD_ID);
+
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+      entries.add(EXAMPLE_ITEM);
+    });
+  }
 }
