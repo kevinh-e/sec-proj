28cfb8876011540ebf1ae56ee40373401cbecc7f
Kevin He <kevin@lyratechnologies.com.au>
2026-06-27 18:47:00 +1000

    more items

--- src/main/java/dev/kevinh/secproj/item/ModItems.java
+++ src/main/java/dev/kevinh/secproj/item/ModItems.java
@@ -9,8 +9,9 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-
   public static final Item EXAMPLE_ITEM = registerItem("example_item", new Item(new Item.Settings()));
+  public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+  public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
 
   private static Item registerItem(String name, Item item) {
     return Registry.register(Registries.ITEM, Identifier.of(SecurityProject.MOD_ID, name), item);
@@ -21,6 +22,8 @@ public class ModItems {
 
     ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
       entries.add(EXAMPLE_ITEM);
+      entries.add(PINK_GARNET);
+      entries.add(RAW_PINK_GARNET);
     });
   }
 }
