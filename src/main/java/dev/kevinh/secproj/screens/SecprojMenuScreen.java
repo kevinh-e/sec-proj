a7f38e4c0c8b69eaefea74206df21de96916ae07
Kevin He <kevin@lyratechnologies.com.au>
2026-06-30 14:11:36 +1000

    screen stub and language

---
+++ src/main/java/dev/kevinh/secproj/screens/SecprojMenuScreen.java
@@ -0,0 +1,11 @@
+package dev.kevinh.secproj.screens;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+public class SecprojMenuScreen extends Screen {
+  public SecprojMenuScreen() {
+    super(Text.translatable("gui.secproj.menu_screen.title"));
+  }
+
+}
