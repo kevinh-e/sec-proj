d963766ae7d1c2e83e00ab478fc41ff3ebd16b6c
Kevin He <kevin@lyratechnologies.com.au>
2026-07-01 11:54:47 +1000

    client player mixin for c2s

---
+++ src/main/java/dev/kevinh/secproj/mixin/ClientPlayerEntityMixin.java
@@ -0,0 +1,10 @@
+package dev.kevinh.secproj.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.client.network.ClientPlayerEntity;
+
+@Mixin(ClientPlayerEntity.class)
+public class ClientPlayerEntityMixin {
+
+}
