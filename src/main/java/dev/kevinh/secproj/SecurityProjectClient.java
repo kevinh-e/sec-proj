e1dd01a7c3cfba53230048c75a62db767db44b94
Kevin He <kevin@lyratechnologies.com.au>
2026-07-03 15:22:48 +1000

    keybinded G to the screen

--- src/main/java/dev/kevinh/secproj/SecurityProjectClient.java
+++ src/main/java/dev/kevinh/secproj/SecurityProjectClient.java
@@ -1,36 +1,55 @@
 package dev.kevinh.secproj;
 
+import org.lwjgl.glfw.GLFW;
+
 import dev.kevinh.secproj.gui.Overlay;
+import dev.kevinh.secproj.gui.SecprojMenuScreen;
 import dev.kevinh.secproj.tools.ClientOptions;
 import dev.kevinh.secproj.tools.TimedQueue;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents.EndTick;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.render.RenderTickCounter;
 
 public class SecurityProjectClient implements ClientModInitializer {
-  public static final TimedQueue clickQueue = new TimedQueue();
   private final MinecraftClient client = MinecraftClient.getInstance();
+
+  public ClientOptions clientOptions = new ClientOptions();
+  public static Overlay overlay = new Overlay();
+  public static final TimedQueue clickQueue = new TimedQueue();
+
+  public KeyBinding OPEN_MENU_KEY;
   private boolean leftMouseWasClicked = false;
   private boolean rightMouseWasClicked = false;
-  public static ClientOptions clientOptions = new ClientOptions();
-  public Overlay overlay = new Overlay();
 
   @Override
   public void onInitializeClient() {
     SecurityProject.LOGGER.info("Setting up " + SecurityProject.MOD_ID + "Client");
 
     overlay.init(clientOptions);
+    this.initializeKeymappings();
+    ClientTickEvents.END_CLIENT_TICK.register(client -> fetchClientTickEvents(client));
 
     // draw shi
     HudRenderCallback.EVENT.register((context, tickDeltaManager) -> {
-      fetchTickEvents(context, tickDeltaManager);
+      fetchRenderTickEvents(context, tickDeltaManager);
       overlay.renderClickerOverlay(context, tickDeltaManager, clickQueue);
     });
   }
 
-  private void fetchTickEvents(DrawContext context, RenderTickCounter tickDeltaManager) {
+  private void fetchClientTickEvents(MinecraftClient tickClient) {
+    if (this.OPEN_MENU_KEY.wasPressed()) {
+      tickClient.setScreen(new SecprojMenuScreen(this.clientOptions));
+    }
+  }
+
+  private void fetchRenderTickEvents(DrawContext context, RenderTickCounter tickDeltaManager) {
     if (this.client.mouse.wasRightButtonClicked()) {
       rightMouseWasClicked = true;
     } else if (rightMouseWasClicked && !this.client.mouse.wasRightButtonClicked()) {
@@ -44,5 +63,10 @@ public class SecurityProjectClient implements ClientModInitializer {
       clickQueue.addEventNow();
       leftMouseWasClicked = false;
     }
+  }
+
+  private void initializeKeymappings() {
+    this.OPEN_MENU_KEY = KeyBindingHelper
+        .registerKeyBinding(new KeyBinding("key.secproj.menu", GLFW.GLFW_KEY_G, "key.categories.secproj"));
   }
 }
