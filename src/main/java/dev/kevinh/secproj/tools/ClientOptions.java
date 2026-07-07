3b139cf846379ba3326513ba96a460785f6866eb
Kevin He <kevin@lyratechnologies.com.au>
2026-07-07 17:42:44 +1000

    add freecam option

--- src/main/java/dev/kevinh/secproj/tools/ClientOptions.java
+++ src/main/java/dev/kevinh/secproj/tools/ClientOptions.java
@@ -3,25 +3,43 @@ package dev.kevinh.secproj.tools;
 public class ClientOptions {
   private boolean autoClickerEnabled;
   private boolean noFallEnabled;
+  private boolean freecamEnabled;
 
   public ClientOptions() {
     autoClickerEnabled = false;
     noFallEnabled = false;
+    freecamEnabled = false;
   }
 
   public void toggleAutoClicker() {
     this.autoClickerEnabled = !this.autoClickerEnabled;
   }
 
-  public void setNoFall(boolean flag) {
-    this.noFallEnabled = flag;
-  }
-
   public boolean isAutoClickerEnabled() {
     return autoClickerEnabled;
   }
 
+  public void setNoFall(boolean flag) {
+    this.noFallEnabled = flag;
+  }
+
+  public void toggleNoFall() {
+    this.noFallEnabled = !this.noFallEnabled;
+  }
+
   public boolean isNoFallEnabled() {
     return noFallEnabled;
+  }
+
+  public boolean isFreecamEnabled() {
+    return freecamEnabled;
+  }
+
+  public void setFreecam(boolean flag) {
+    this.freecamEnabled = flag;
+  }
+
+  public void toggleFreecam() {
+    this.freecamEnabled = !this.freecamEnabled;
   }
 }
