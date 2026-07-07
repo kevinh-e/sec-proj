94ef56bf3dbc612c97998db20f5bb8c390a57b6f
Kevin He <kevin@lyratechnologies.com.au>
2026-07-07 17:42:57 +1000

    make overlay render more scalable with a list

--- src/main/java/dev/kevinh/secproj/gui/Overlay.java
+++ src/main/java/dev/kevinh/secproj/gui/Overlay.java
@@ -1,5 +1,7 @@
 package dev.kevinh.secproj.gui;
 
+import java.util.ArrayList;
+
 import dev.kevinh.secproj.SecurityProjectClient;
 import dev.kevinh.secproj.tools.TimedQueue;
 import net.minecraft.client.MinecraftClient;
@@ -10,23 +12,36 @@ import net.minecraft.client.render.RenderTickCounter;
 public class Overlay {
   private static final int TEXT_COLOUR = 0xFFFFFFFF;
   private static final int PADDING_SIZE = 5;
+  private static final int STARTING_Y = 130;
 
-  public static void renderClickerOverlay(DrawContext context, RenderTickCounter tickDeltaManager,
+  public static void renderOverlay(DrawContext context, RenderTickCounter tickDeltaManager,
       TimedQueue clickQueue) {
     TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
-    String cpsCopy = "CPS " + clickQueue.size();
-    String autoClickerCopy = "AutoClicker "
-        + (SecurityProjectClient.getClientOptions().isAutoClickerEnabled() ? "enabled" : "disabled");
 
-    int textWidth = Math.max(textRenderer.getWidth(cpsCopy), textRenderer.getWidth(autoClickerCopy));
+    ArrayList<String> lines = new ArrayList<String>();
+
+    lines.add("CPS " + clickQueue.size());
+    lines.add("AutoClicker "
+        + (SecurityProjectClient.getClientOptions().isAutoClickerEnabled() ? "enabled" : "disabled"));
+    lines.add("Freecam "
+        + (SecurityProjectClient.getClientOptions().isFreecamEnabled() ? "enabled" : "disabled"));
+
+    int textWidth = lines.stream().mapToInt(textRenderer::getWidth).max().orElse(0);
     int textHeight = textRenderer.fontHeight + PADDING_SIZE;
 
     // Background
-    context.fill(5, 155, textWidth + 3 * PADDING_SIZE, 155 + 2 * (textHeight + PADDING_SIZE), 0, 0xBF3A3632);
-    context.drawBorder(5, 155, textWidth + 2 * PADDING_SIZE, 2 * (textHeight + PADDING_SIZE), 0xBF57534E);
+    context.fill(5, STARTING_Y, textWidth + (lines.size() + 1) * PADDING_SIZE,
+        STARTING_Y + lines.size() * (textHeight + PADDING_SIZE), 0,
+        0xBF3A3632);
+    context.drawBorder(5, STARTING_Y, textWidth + lines.size() * PADDING_SIZE,
+        lines.size() * (textHeight + PADDING_SIZE),
+        0xBF57534E);
 
     // Text
-    context.drawText(textRenderer, cpsCopy, 10, 160, TEXT_COLOUR, false);
-    context.drawText(textRenderer, autoClickerCopy, 10, 160 + PADDING_SIZE + textHeight, TEXT_COLOUR, false);
+    for (String line : lines) {
+      context.drawText(textRenderer, line, 10,
+          STARTING_Y + PADDING_SIZE + lines.indexOf(line) * (PADDING_SIZE + textHeight), TEXT_COLOUR,
+          false);
+    }
   }
 }
