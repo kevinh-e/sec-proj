2219d2fe8929423d083a2e30b3373ffeb15d880d
kevinh-e <account@kevinh.dev>
2026-07-23 19:04:56 +1000

    update box to be absolute position in the world

--- src/main/java/dev/kevinh/secproj/tools/Render3d.java
+++ src/main/java/dev/kevinh/secproj/tools/Render3d.java
@@ -7,9 +7,13 @@ import net.minecraft.util.math.Box;
 
 public class Render3d {
   public static void drawTestBox(WorldRenderContext ctx) {
-    Box box = new Box(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+    double camX = ctx.camera().getPos().x;
+    double camY = ctx.camera().getPos().y;
+    double camZ = ctx.camera().getPos().z;
+
+    Box box = new Box(-camX, -camY, -camZ, -camX + 1.0, -camY + 1.0, -camZ + 1.0);
     WorldRenderer.drawBox(ctx.matrixStack(), ctx.consumers().getBuffer(RenderLayer.getLines()), box, (float) 1.0,
-        (float) 1.0,
+        (float) 0.0,
         (float) 1.0, (float) 1.0);
   }
 }
